ae6fef52766112ee8bfc601dca0e296d42a4d6cc
ctk12 <chetankatwa12@gmail.com>
2024-08-22 04:35:32 +0530

    update

--- components/Forms.tsx
+++ components/Forms.tsx
@@ -114,8 +114,8 @@ export function InfoForm({ data }: { data: FormDataType}) {
 
 const CustomInput = ({ inputName, inputId, formErrors, value }: { inputName: string, inputId: string, formErrors?: FormErrorsType, value?: string }) => {
     return (
-        <div className="flex flex-wrap -mx-3 mb-6">
-          <div className="w-full px-3 mb-6 md:mb-0">
+        <div className="flex flex-wrap -mx-3 mb-2">
+          <div className="w-full px-3 mb-2 md:mb-0">
             <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor={inputId}>
               {inputName}
             </label>
